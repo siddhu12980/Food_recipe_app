0b654d638c5a6e158d26f00188aed8bf61b4fe3e
Siddhanta Acharya <siddhu12980@gmail.com>
2023-09-30 22:55:46 +0530

    day_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:third_proj2/HomeScreen.dart';
+import 'HomeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
